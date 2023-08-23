bb83d82d348a6d7d2ecf1fe97d9284774d3a8c9d
Anubhav Sigdel <64096825+asigdel29@users.noreply.github.com>
2023-08-23 12:47:37 -0400

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,123 +1,67 @@
-from datetime import time
+from beamngpy import BeamNGpy, Vehicle, Scenario
+from beamngpy.sensors import Camera, Ultrasonic
+import time
 
-import numpy as np
-from beamngpy import BeamNGpy, Scenario, Vehicle, set_up_simple_logging
-from beamngpy.sensors import ultrasonic
+# Initialize BeamNGpy instance
+beamng = BeamNGpy('localhost', 64256, 'C:/Users/sigde/OneDrive/Desktop/BeamNG.tech.v0.28.2.0/BeamNG.tech.v0.28.2.0')
 
-SIZE = 1024
+# Define the scenario
+scenario = Scenario('west_coast_usa', 'CACC', description='Collaborative Adaptive Cruise Control')
 
-def main():
-    set_up_simple_logging()
+# Create and configure the lead vehicle
+lead_vehicle = Vehicle('lead_vehicle', model='etk800', licence='LEAD', colour='Red')
+camera_lead = Camera(beamng, 'camera_lead', pos=(0, 1, 1.5))
+ultrasonic_lead = Ultrasonic(pos=(0, 2.5, 0.4))
+lead_vehicle.attach_sensor('camera_lead', camera_lead)
+lead_vehicle.attach_sensor('ultrasonic_lead', ultrasonic_lead)
 
-    beamng = BeamNGpy('localhost', 64256, 'C:/Users/ayusa/OneDrive/Desktop/BeamNG.tech.v0.27.1.0/BeamNG.tech.v0.27.1.0')
-    beamng.open(launch=True)
+# Add the lead vehicle to the scenario
+scenario.add_vehicle(lead_vehicle, pos=(0, 0, 0), rot=None, rot_quat=None)
 
-    orig = (-769.1, 400.8, 142.8)
+# Create and configure the follow vehicles
+for i in range(1, 11):
+    follow_vehicle = Vehicle(f'follow_vehicle_{i}', model='etk800', licence=f'FOLLOW{i}', colour='Blue')
+    follow_vehicle.ai_set_mode('span')
+    camera_follow = Camera(beamng, f'camera_follow_{i}', pos=(0, 1, 1.5))
+    ultrasonic_follow = Ultrasonic(pos=(0, 2.5, 0.4))
+    follow_vehicle.attach_sensor(f'camera_follow_{i}', camera_follow)
+    follow_vehicle.attach_sensor(f'ultrasonic_follow_{i}', ultrasonic_follow)
+    scenario.add_vehicle(follow_vehicle, pos=(i*5, i*5, 0), rot=None, rot_quat=None)
 
-    script = []
-    points = []
-    point_color = [0, 0, 0, 0.1]
-    sphere_coordinates = []
-    sphere_radii = []
-    sphere_colors = []
+# Compile the scenario
+scenario.make(beamng)
 
-    for i in range(3600):
-        node = {
-            #  Calculate the position as a sine curve that makes the vehicle
-            #  drive from left to right. The z-coordinate is not calculated in
-            #  any way because `ai_set_script` by default makes the polyline to
-            #  follow cling to the ground, meaning the z-coordinate will be
-            #  filled in automatically.
-            'x': 4 * np.sin(np.radians(i)) + orig[0],
-            'y': i * 0.2 + orig[1],
-            'z': orig[2],
-            #  Calculate timestamps for each node such that the speed between
-            #  points has a sinusoidal variance to it.
-            't': (2 * i + (np.abs(np.sin(np.radians(i)))) * 64) / 64,
-        }
-        script.append(node)
-        points.append((node['x'], node['y'], node['z']))
+# Start BeamNG and enter the created scenario
+bng = beamng.open(launch=True)
+try:
+    bng.load_scenario(scenario)
+    bng.start_scenario()
 
-        if i % 10 == 0:
-            sphere_coordinates.append((node['x'], node['y'], node['z']))
-            sphere_radii.append(np.abs(np.sin(np.radians(i))) * 0.25)
-            sphere_colors.append((np.sin(np.radians(i)), 0, 0, 0.8))
+    # Set lead vehicle AI mode to 'span' (driving at random points)
+    lead_vehicle.ai_set_mode('span')
 
-    scenario = Scenario('west_coast_usa', 'ai_test')
+    # Poll sensor data for a specified duration (e.g., 60 seconds)
+    for _ in range(60):
+        # Poll data from the lead vehicle's sensors
+        sensors_lead = bng.poll_sensors(lead_vehicle)
+        print('Lead vehicle camera data:', sensors_lead['camera_lead'])
+        print('Lead vehicle ultrasonic data:', sensors_lead['ultrasonic_lead'])
 
-    vehicle = Vehicle('ego_vehicle', model='etk800', license='AI')
-    vehicle1 = Vehicle('ego_vehicle1', model='etk800', license='AI')
-    vehicle2 = Vehicle('ego_vehicle2', model='etk800', license='AI')
-    vehicle3 = Vehicle('ego_vehicle3', model='etk800', license='AI')
-    vehicle4 = Vehicle('ego_vehicle4', model='etk800', license='AI')
-    vehicle5 = Vehicle('ego_vehicle5', model='etk800', license='AI')
+        # Poll data from the follow vehicles' sensors and check for contact
+        for i in range(1, 11):
+            follow_vehicle = scenario.get_vehicle_by_name(f'follow_vehicle_{i}')
+            sensors_follow = bng.poll_sensors(follow_vehicle)
+            print(f'Follow vehicle {i} camera data:', sensors_follow[f'camera_follow_{i}'])
+            print(f'Follow vehicle {i} ultrasonic data:', sensors_follow[f'ultrasonic_follow_{i}'])
 
-    scenario.add_vehicle(vehicle, pos=(-769.1, 400.8, 142.8), rot_quat=(0, 0, 1, 0))
-    scenario.add_vehicle(vehicle1, pos=(-769.1, 395.8, 142.8), rot_quat=(0, 0, 1, 0))
-    scenario.add_vehicle(vehicle2, pos=(-769.1, 390.8, 142.8), rot_quat=(0, 0, 1, 0))
-    scenario.add_vehicle(vehicle3, pos=(-769.1, 385.8, 142.8), rot_quat=(0, 0, 1, 0))
-    scenario.add_vehicle(vehicle4, pos=(-769.1, 380.8, 142.8), rot_quat=(0, 0, 1, 0))
-    scenario.add_vehicle(vehicle5, pos=(-769.1, 375.8, 142.8), rot_quat=(0, 0, 1, 0))
+            # Check for contact with the lead vehicle using the ultrasonic sensor
+            if sensors_follow[f'ultrasonic_follow_{i}']['distance'] < 1.5:
+                # If contact detected, set AI mode to 'chase' to follow the target car
+                follow_vehicle.ai_set_target(lead_vehicle.vid)
+                follow_vehicle.ai_set_mode('chase')
 
-    scenario.make(beamng)
+        # Wait for a second before polling data again
+        time.sleep(1)
 
-    beamng.settings.set_deterministic(60)
-
-    try:
-        beamng.scenario.load(scenario)
-        vehicle.attach_sensor("ultrasonic", ultrasonic)
-        vehicle1.attach_sensor("ultrasonic", ultrasonic)
-        vehicle2.attach_sensor("ultrasonic", ultrasonic)
-        vehicle3.attach_sensor("ultrasonic", ultrasonic)
-        vehicle4.attach_sensor("ultrasonic", ultrasonic)
-        vehicle5.attach_sensor("ultrasonic", ultrasonic)
-
-        beamng.scenario.start()
-        beamng.debug.add_spheres(sphere_coordinates, sphere_radii, sphere_colors, cling=True, offset=0.1)
-        beamng.debug.add_polyline(points, point_color, cling=True, offset=0.1)
-        vehicle.ai.set_script(script)
-        vehicle1.ai.set_script(script)
-        vehicle2.ai.set_script(script)
-        vehicle3.ai.set_script(script)
-        vehicle4.ai.set_script(script)
-        vehicle5.ai.set_script(script)
-
-        distance_threshold = 5.0  # meters
-        stop_time = 1.0  # seconds
-
-        while True:
-            distance1 = vehicle1.get_sensor("ultrasonic")["distance"]
-            distance2 = vehicle2.get_sensor("ultrasonic")["distance"]
-            distance3 = vehicle3.get_sensor("ultrasonic")["distance"]
-            distance4 = vehicle4.get_sensor("ultrasonic")["distance"]
-            distance5 = vehicle5.get_sensor("ultrasonic")["distance"]
-
-            if distance1 < distance_threshold:
-                vehicle1.ai.stop()
-                time.sleep(stop_time)
-                vehicle1.ai.resume()
-            if distance2 < distance_threshold:
-                vehicle2.ai.stop()
-                time.sleep(stop_time)
-                vehicle2.ai.resume()
-            if distance3 < distance_threshold:
-                vehicle3.ai.stop()
-                time.sleep(stop_time)
-                vehicle3.ai.resume()
-            if distance4 < distance_threshold:
-                vehicle4.ai.stop()
-                time.sleep(stop_time)
-                vehicle4.ai.resume()
-            if distance5 < distance_threshold:
-                vehicle5.ai.stop()
-                time.sleep(stop_time)
-                vehicle5.ai.resume()
-
-            beamng.control.step(60)
-    finally:
-        beamng.close()
-
-
-
-if __name__ == '__main__':
-    main()
+finally:
+    bng.close()
